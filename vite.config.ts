9cbb514eb299fc23e3c1cbb2fa0c2bbf16ecf778
NI0317 <stniii0317@gmail.com>
2025-04-24 00:35:48 +0800

    fix: 修复 GitHub Pages 资源加载路径

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   
   return {
-    base: '/',
+    base: '/silent-witness-interactive-web/',
     server: {
       host: "localhost",
       port: 8080,
